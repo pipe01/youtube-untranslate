d6e6286c49d89d494ada86e397c9cbac20f7bf5f
Felipe Martinez <pipernene@gmail.com>
2021-09-07 19:42:29 +0200

    Add icons

--- build.js
+++ build.js
@@ -4,6 +4,7 @@ const path = require("path");
 const fs = require("fs");
 
 const watch = process.argv.includes("--watch");
+const includeFiles = ["manifest.json", "icons/48x48.png", "icons/96x96.png"];
 
 try {
     fs.mkdirSync("artifacts");
@@ -13,20 +14,21 @@ const version = JSON.parse(fs.readFileSync("manifest.json")).version;
 
 esbuild.build({
     entryPoints: ["src/content.ts"],
-    outfile: "content.js",
+    outfile: "dist/content.js",
     bundle: true,
     format: "cjs",
     watch,
     minify: !watch,
     metafile: true,
-    write: false
+    write: watch
 })
 .then(o => {
     if (!watch) {
         const zip = new JSZip();
 
         o.outputFiles.forEach(file => zip.file("dist/" + path.basename(file.path), file.contents));
-        zip.file("manifest.json", fs.readFileSync("manifest.json"));
+
+        includeFiles.forEach(o => zip.file(o, fs.readFileSync(o)));
 
         zip.generateAsync({type: "arraybuffer"}).then(o => {
             fs.writeFileSync(`artifacts/extension-${version}.zip`, Buffer.from(o));
