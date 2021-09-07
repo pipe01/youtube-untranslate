36ad72cc99da53368b4db9a70df65a63c146bee1
Felipe Martinez <pipernene@gmail.com>
2021-09-07 18:26:24 +0200

    Build zip file on build script

--- build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-cd src
-node build.js || pause
-cd ..
-web-ext build --ignore-files src/**/* build.bat --overwrite-dest || pause
--- build.js
+++ build.js
@@ -1,12 +1,35 @@
 const esbuild = require("esbuild");
+const JSZip = require("jszip");
+const path = require("path");
+const fs = require("fs");
 
 const watch = process.argv.includes("--watch");
 
+try {
+    fs.mkdirSync("artifacts");
+} catch (e) {}
+
+const version = JSON.parse(fs.readFileSync("manifest.json")).version;
+
 esbuild.build({
-    entryPoints: ["content.ts"],
-    outfile: "../dist/content.js",
+    entryPoints: ["src/content.ts"],
+    outfile: "content.js",
     bundle: true,
     format: "cjs",
     watch,
-    minify: !watch
+    minify: !watch,
+    metafile: true,
+    write: false
+})
+.then(o => {
+    if (!watch) {
+        const zip = new JSZip();
+
+        o.outputFiles.forEach(file => zip.file("dist/" + path.basename(file.path), file.contents));
+        zip.file("manifest.json", fs.readFileSync("manifest.json"));
+
+        zip.generateAsync({type: "arraybuffer"}).then(o => {
+            fs.writeFileSync(`artifacts/extension-${version}.zip`, Buffer.from(o));
+        })
+    }
 })
