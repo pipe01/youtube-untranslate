68f705abeaf8ee15fa33894eb64c6b85487b1559
Felipe Martinez <pipernene@gmail.com>
2021-09-07 16:44:24 +0200

    Add extension

---
+++ src/content.ts
@@ -0,0 +1,86 @@
+import "regenerator-runtime"
+
+let titleElement: HTMLElement | null = null;
+let textElement: HTMLElement | null = null;
+let originalTitle: string;
+let translatedTitle: string;
+let showingTranslated = true;
+
+async function load() {
+    if (!titleElement) {
+        return;
+    }
+
+    console.log(translatedTitle);
+
+    const data = await fetch(`https://www.youtube.com/oembed?format=json&url=${encodeURIComponent(location.href)}`).then(o => o.json());
+    originalTitle = data.title;
+
+    if (originalTitle !== translatedTitle) {
+        originalTitle = data.title;
+
+        // titleElement.innerHTML = "";
+
+        // textElement = document.createElement("span");
+        // titleElement.appendChild(textElement);    
+
+        // const toggler = document.createElement("a");
+        // toggler.href = "#";
+        // toggler.innerText = "Show translation";
+        // toggler.classList.add("yt-simple-endpoint", "style-scope", "yt-formatted-string");
+        // toggler.style.marginLeft = ".5rem";
+        // toggler.style.fontSize = ".7em";
+        // toggler.onclick = e => {
+        //     e.preventDefault();
+        //     toggler.innerText = showingTranslated ? "Show original" : "Show translation";
+
+        //     textElement!.innerText = showingTranslated ? translatedTitle : originalTitle;
+
+        //     showingTranslated = !showingTranslated;
+        // }
+        // titleElement.appendChild(toggler);
+
+        // textElement.innerText = originalTitle;
+
+        titleElement.innerText = originalTitle;
+        titleElement.style.color = "rgb(164, 221, 255)";
+        titleElement.style.cursor = "pointer";
+        titleElement.onclick = () => {
+            titleElement!.innerText = showingTranslated ? translatedTitle : originalTitle;
+            showingTranslated = !showingTranslated;
+        }
+    } else {
+        titleElement.innerHTML = "";
+        titleElement.innerText = originalTitle;
+        titleElement.style.color = "";
+        titleElement.style.cursor = "";
+        titleElement.onclick = null;
+    }
+}
+
+const getScriptTag = () => document.getElementById("scriptTag")?.innerText;
+
+let currentScriptTag: string | undefined;
+setInterval(() => {
+    if (getScriptTag() != currentScriptTag) {
+        currentScriptTag = getScriptTag();
+
+        if (location.pathname === "/watch" && currentScriptTag) {
+            translatedTitle = JSON.parse(currentScriptTag).name;
+            refresh();
+        }
+    }
+}, 300);
+
+function refresh() {
+    const params = new URLSearchParams(location.search);
+    if (params.has("v")) {
+        titleElement = document.querySelector<HTMLElement>("h1.title > *");
+        if (titleElement) {
+            showingTranslated = true;
+            load();
+        } else {
+            setTimeout(refresh, 100);
+        }
+    }
+}
